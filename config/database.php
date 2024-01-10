34e48cb24d1f9917512866b8d2206ffaa1b05ad4
ilestis <jay.payne.ch@gmail.com>
2024-01-10 16:22:32 -0600

    DB config r/w

--- config/database.php
+++ config/database.php
@@ -46,6 +46,12 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
+            'read' => [
+                'host' => env('DB_HOST_RW', env('DB_HOST', '127.0.0.1')),
+            ],
+            'write' => [
+                'host' => env('DB_HOST_RW', env('DB_HOST', '127.0.0.1')),
+            ],
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
